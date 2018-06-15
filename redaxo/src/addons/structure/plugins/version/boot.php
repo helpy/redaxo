a7c21fd59046208a31935e8c8b14c0a24803c0a4
Guido Erlinger <dev@helpy.de>
2018-06-15 23:41:33 +0200

    First changes of function bar of version plugin.

--- redaxo/src/addons/structure/plugins/version/boot.php
+++ redaxo/src/addons/structure/plugins/version/boot.php
@@ -15,7 +15,7 @@ rex_perm::register('version[live_version]', null, rex_perm::OPTIONS);
 // ***** an EPs andocken
 rex_extension::register('ART_INIT', function (rex_extension_point $ep) {
     $version = rex_request('rex_version', 'int');
-    if ($version != 1) {
+    if ($version != rex_article_revision::WORK) {
         return;
     }
 
@@ -44,23 +44,23 @@ rex_extension::register('STRUCTURE_CONTENT_HEADER', function (rex_extension_poin
 
     $working_version_empty = true;
     $gw = rex_sql::factory();
-    $gw->setQuery('select * from ' . rex::getTablePrefix() . 'article_slice where article_id=? and clang_id=? and revision=1 LIMIT 1', [$params['article_id'], $params['clang']]);
+    $gw->setQuery('select * from ' . rex::getTablePrefix() . 'article_slice where article_id=? and clang_id=? and revision=' . rex_article_revision::WORK . ' LIMIT 1', [$params['article_id'], $params['clang']]);
     if ($gw->getRows() > 0) {
         $working_version_empty = false;
     }
 
-    $revisions = [];
-    $revisions[0] = rex_i18n::msg('version_liveversion');
-    $revisions[1] = rex_i18n::msg('version_workingversion');
+    // $revisions = [];
+    // $revisions[0] = rex_i18n::msg('version_liveversion');
+    // $revisions[1] = rex_i18n::msg('version_workingversion');
 
     $version_id = rex_request('rex_set_version', 'int', '-1');
 
-    if ($version_id === 0) {
-        $rex_version_article[$params['article_id']] = 0;
-    } elseif ($version_id == 1) {
-        $rex_version_article[$params['article_id']] = 1;
+    if ($version_id === rex_article_revision::LIVE) {
+        $rex_version_article[$params['article_id']] = rex_article_revision::LIVE;
+    } elseif ($version_id == rex_article_revision::WORK) {
+        $rex_version_article[$params['article_id']] = rex_article_revision::WORK;
     } elseif (!isset($rex_version_article[$params['article_id']])) {
-        $rex_version_article[$params['article_id']] = 1;
+        $rex_version_article[$params['article_id']] = rex_article_revision::WORK;
     }
 
     $func = rex_request('rex_version_func', 'string');
@@ -80,8 +80,8 @@ rex_extension::register('STRUCTURE_CONTENT_HEADER', function (rex_extension_poin
     }
 
     if (!rex::getUser()->hasPerm('version[live_version]')) {
-        $rex_version_article[$params['article_id']] = 1;
-        unset($revisions[0]);
+        $rex_version_article[$params['article_id']] = rex_article_revision::WORK;
+        // unset($revisions[0]);
     }
 
     rex::getProperty('login')->setSessionVar('rex_version_article', $rex_version_article);
@@ -93,6 +93,7 @@ rex_extension::register('STRUCTURE_CONTENT_HEADER', function (rex_extension_poin
         'ctype' => $params['ctype'],
     ]);
 
+/*
     $items = [];
     $brand = '';
     foreach ($revisions as $version => $revision) {
@@ -107,7 +108,9 @@ rex_extension::register('STRUCTURE_CONTENT_HEADER', function (rex_extension_poin
     }
 
     $toolbar = '';
+*/
 
+/* 
     $fragment = new rex_fragment();
     $fragment->setVar('button_prefix', rex_i18n::msg('version'));
     $fragment->setVar('items', $items, false);
@@ -118,27 +121,193 @@ rex_extension::register('STRUCTURE_CONTENT_HEADER', function (rex_extension_poin
     }
 
     $toolbar .= '<li class="dropdown">' . $fragment->parse('core/dropdowns/dropdown.php') . '</li>';
+*/
+    $btn_icons = array(
+        'draft_state'       => 'square-o',
+        'live_state'        => 'check-square-o',
+        'copy_work_to_live' => 'share-square-o',
+        'copy_live_to_work' => 'pencil-square-o',
+        'preview-work'      => 'eye-slash',
+        'preview-live'      => 'eye',
+    );
 
-    if (!rex::getUser()->hasPerm('version[live_version]')) {
-        if ($rex_version_article[$params['article_id']] > 0) {
-            $toolbar .= '<li><a href="' . $context->getUrl(['rex_version_func' => 'copy_live_to_work']) . '">' . rex_i18n::msg('version_copy_from_liveversion') . '</a></li>';
-            $toolbar .= '<li><a href="' . rex_getUrl($params['article_id'], $params['clang'], ['rex_version' => 1]) . '" rel="noopener noreferrer" target="_blank">' . rex_i18n::msg('version_preview') . '</a></li>';
-        }
-    } else {
-        if ($rex_version_article[$params['article_id']] > 0) {
-            if (!$working_version_empty) {
-                $toolbar .= '<li><a href="' . $context->getUrl(['rex_version_func' => 'copy_work_to_live']) . '">' . rex_i18n::msg('version_working_to_live') . '</a></li>';
-            }
-            $toolbar .= '<li><a href="' . rex_getUrl($params['article_id'], $params['clang'], ['rex_version' => 1]) . '" rel="noopener noreferrer" target="_blank">' . rex_i18n::msg('version_preview') . '</a></li>';
+    $btn_toolbar = array(
+        'vertical'    => false,
+        'size'        => '',
+        'btn_groups'  => [],
+    );
+    if (rex::getUser()->hasPerm('version[live_version]')) {
+        if ($rex_version_article[$params['article_id']] > rex_article_revision::LIVE) {
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['draft_state'];
+            $btn_item['text'] = rex_i18n::msg('version_workingversion');
+            $btn_item['url']   = $context->getUrl(['rex_set_version' => rex_article_revision::WORK]);
+            $btn_item['attributes']['class'][] = 'btn-warning';
+            $btn_item['attributes']['title'] = '';
+            $btn_group[] = $btn_item;
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['live_state'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = $context->getUrl(['rex_set_version' => rex_article_revision::LIVE]);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_switch_to_live');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
         } else {
-            $toolbar .= '<li><a href="' . $context->getUrl(['rex_version_func' => 'copy_live_to_work']) . '" data-confirm="' . rex_i18n::msg('version_confirm_copy_live_to_workingversion') . '">' . rex_i18n::msg('version_copy_live_to_workingversion') . '</a></li>';
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['draft_state'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = $context->getUrl(['rex_set_version' => rex_article_revision::WORK]);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_switch_to_work');
+            $btn_group[] = $btn_item;
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['live_state'];
+            $btn_item['text'] = rex_i18n::msg('version_liveversion');
+            $btn_item['url']   = $context->getUrl(['rex_set_version' => rex_article_revision::LIVE]);
+            $btn_item['attributes']['class'][] = 'btn-success';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_switch_to_work');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
         }
     }
 
-    $inverse = $rex_version_article[$params['article_id']] == 1 ? true : false;
-    $cssClass = $rex_version_article[$params['article_id']] == 1 ? 'rex-state-inprogress' : 'rex-state-live';
+    if (!rex::getUser()->hasPerm('version[live_version]')) {
+        if ($rex_version_article[$params['article_id']] > rex_article_revision::LIVE) {
+            $btn_group = [];
 
-    $return .= rex_view::toolbar('<ul class="nav navbar-nav">' . $toolbar . '</ul>', $brand, $cssClass, $inverse);
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['draft_state'];
+            $btn_item['text'] = rex_i18n::msg('version_workingversion');
+            $btn_item['url']   = '#';
+            $btn_item['attributes']['class'][] = 'btn-warning';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_work_only');
+            $btn_group[] = $btn_item;
+            $btn_toolbar['btn_groups'][] = $btn_group;
+
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['copy_live_to_work'];
+            $btn_item['text'] = rex_i18n::msg('version_copy_from_liveversion');
+            $btn_item['url']   = $context->getUrl(['rex_version_func' => 'copy_live_to_work']);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_copy_live_to_work');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
+
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['preview-work'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = rex_getUrl($params['article_id'], $params['clang'], ['rex_version' => rex_article_revision::WORK]);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['rel'] = 'noopener noreferrer';
+            $btn_item['attributes']['target'] = '_blank';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_preview');
+            $btn_group[] = $btn_item;
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['preview-live'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = rex_getUrl($params['article_id'], $params['clang']);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['rel'] = 'noopener noreferrer';
+            $btn_item['attributes']['target'] = '_blank';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_preview');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
+
+        }
+    } else {
+        if ($rex_version_article[$params['article_id']] > rex_article_revision::LIVE) {
+            $btn_group = [];
+            if (!$working_version_empty) {
+                $btn_item = [];
+                $btn_item['symbol'] = $btn_icons['copy_work_to_live'];
+                $btn_item['text'] = rex_i18n::msg('version_working_to_live');
+                $btn_item['url']   = $context->getUrl(['rex_version_func' => 'copy_work_to_live']);
+                $btn_item['attributes']['class'][] = 'btn-default';
+                $btn_item['attributes']['title'] = rex_i18n::msg('version_title_copy_work_to_live');
+                $btn_group[] = $btn_item;
+            }
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['copy_live_to_work'];
+            $btn_item['text'] = rex_i18n::msg('version_copy_from_liveversion');
+            $btn_item['url']   = $context->getUrl(['rex_version_func' => 'copy_live_to_work']);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_copy_live_to_work');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
+
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['preview-work'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = rex_getUrl($params['article_id'], $params['clang'], ['rex_version' => rex_article_revision::WORK]);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['rel'] = 'noopener noreferrer';
+            $btn_item['attributes']['target'] = '_blank';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_preview');
+            $btn_group[] = $btn_item;
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['preview-live'];
+            $btn_item['text'] = '';
+            $btn_item['url']   = rex_getUrl($params['article_id'], $params['clang']);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['rel'] = 'noopener noreferrer';
+            $btn_item['attributes']['target'] = '_blank';
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_preview');
+            $btn_group[] = $btn_item;
+
+            $btn_toolbar['btn_groups'][] = $btn_group;
+        } else {
+            $btn_group = [];
+
+            $btn_item = [];
+            $btn_item['symbol'] = $btn_icons['copy_live_to_work'];
+            $btn_item['text'] = rex_i18n::msg('version_copy_live_to_workingversion');
+            $btn_item['url']   = $context->getUrl(['rex_version_func' => 'copy_live_to_work']);
+            $btn_item['attributes']['class'][] = 'btn-default';
+            $btn_item['attributes']['data-confirm'] = rex_i18n::msg('version_confirm_copy_live_to_workingversion');
+            $btn_item['attributes']['title'] = rex_i18n::msg('version_title_copy_live_to_work');
+            $btn_group[] = $btn_item;
+            $btn_toolbar['btn_groups'][] = $btn_group;
+        }
+    }
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('vertical', $btn_toolbar['vertical']);
+    $fragment->setVar('size', $btn_toolbar['size']);
+
+    $btn_group_parsed = '';
+    //$visible_class = 'visible-lg-inline visible-md-inline visible-sm-inline';
+    foreach($btn_toolbar['btn_groups'] as &$buttons) {
+        foreach($buttons as &$button) {
+            $button['label'] = 
+                '<i class="fa fa-' . $button['symbol'] . '"></i>' . 
+                '<span class="' . $visible_class . '"> ' . $button['text'] . '</span>';
+        }
+        $fragment->setVar('buttons', $buttons, false);
+        $btn_group_parsed .= $fragment->parse('core/buttons/button_group.php');
+        $visible_class = 'visible-lg-inline visible-md-inline visible-sm-inline';
+        // $visible_class = 'visible-lg-inline';
+    }
+    $return .= '<div class="nav rex-page-nav"><div class="btn-toolbar" role="toolbar" aria-label="version toolbar">'.$btn_group_parsed.'</div></div>';
 
     $params['slice_revision'] = $rex_version_article[$params['article_id']];
 
